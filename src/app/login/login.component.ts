54b3f518147683b31c07a1284d8eb2d3299f6b7e
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-31 15:35:38 -0300

    Adicionado funcionalidade login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: any
 
-  ngOnInit(): void {
+  // Dados fictícios para simular validação
+  emailDB: string = "email@teste.com";
+  senhaDB: string = "12345678";
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      senha: ['', Validators.required]
+    })
+  }
+
+  login() {
+    console.log('passei no metodo')
+    const email = this.loginForm.get('email').value
+    const senha = this.loginForm.get('senha').value
+
+    // Validação simples apra simular
+    if (this.loginForm.get('email').value == this.emailDB && this.loginForm.get('senha').value == this.senhaDB) {
+      console.log('Login com sucesso!!!')
+      this.router.navigate(['home'])
+    } else {
+      console.log("Dados incorretos")
+    }
   }
 
 }
